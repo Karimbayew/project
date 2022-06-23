cead6a956a2a7a7d963a23e06187d6fc54d609ac
Shahzad <karimbayewshahzad@gmail.com>
2022-06-24 02:17:52 +0500

    firstCommit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMoveiDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const answerFilms = prompt("Один из последних просмотренных фильмов?"),
+      answerFilmsBall = prompt("На сколько оцените его?");
+
+personalMoveiDB.movies[answerFilms] = answerFilmsBall;
+
+console.log(personalMoveiDB);   
